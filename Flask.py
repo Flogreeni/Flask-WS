b48f5934d8f0756d1254bcbd398a2380ea75a885
Florian <florian.gruenbaum@gmx.de>
2021-12-04 14:28:31 +0100

    stage 2

--- Flask.py
+++ Flask.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, from TechLabs!</p>"
+
+
+@app.route("/hello/")
+def hello():
+    return f"<p>hello!</p>"
